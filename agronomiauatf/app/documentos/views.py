28e7f19aecd921df7f6cbeff9fa3e5a03287688d
jjavier <javier.limachi@a2odev.com>
2021-01-31 09:03:05 -0400

    mejore la informacion de los documentos

--- agronomiauatf/app/documentos/views.py
+++ agronomiauatf/app/documentos/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django import forms
 from django.urls import reverse, reverse_lazy
-#from django.core.urlresolvers import reverse_lazy
+from django.db.models import Q
 # Create your views here.
 
 class ListarCategorias(ListView):
@@ -72,9 +72,27 @@ def CreateDocumento(request):
     return render(request,'documentos/CreateDocumento.html',{'form':forms})
 
 def shearDocuments(request):
-    pass
+    return render(request,'documentos/shearDocuments.html',{})
 
-
+def BuscarDocumento_view(request):
+    if request.method=="POST":
+        texto=request.POST["name_documents"]
+        busqueda=(
+            Q(Titulo__icontains=texto) |
+            Q(Fecha_creacion__icontains=texto) |
+            Q(id__icontains=texto)
+        )
+        resultados=Documento.objects.filter(busqueda, estado=True).distinct()
+        return render(request,'documentos/searList.html',{'object_list':resultados})
+    else:
+        texto=request.GET["name_documents"]
+        busqueda=(
+            Q(Titulo__icontains=texto) |
+            Q(Titulo__icontains=texto) |
+            Q(id__icontains=texto)
+        )
+        resultados=Documento.objects.filter(busqueda, estado=True).distinct()
+        return render(request,'documentos/searList.html',{'object_list':resultados}) 
 class DetailDocuments(DetailView):
     model = Documento
     template_name = "documentos/DocumentoDetailView.html"
@@ -193,7 +211,18 @@ class ListComentarios(ListView):
     def get(self, request, *args, **kwargs):
         self.dic = {
             'object_list':self.model.objects.filter(documento_id = int(kwargs['id_document'])).order_by('-id')
-            #'usuarios':self.model.objects.all().count()
         }
         return render(request,self.template_name, self.dic)
-        
+
+
+
+class ComentarDeleteView(DeleteView):
+    model = Comentar
+    template_name = "documentos/ComentarDeleteView.html"
+    def post(self, request, *args, **kwargs):
+        #print(request.POST['option'])#obtengo un dato de un input del formulario
+        #print(kwargs['pk'])#diccionario de datos para obtener los datos del la url
+        get_data = self.model.objects.get(pk = kwargs['pk'])
+        get_data.estado = False 
+        get_data.save()
+        return HttpResponse('200')
